53da3bd646c61b8cc8950c318ff9b3fba09a156d
mike andrews <mra@xoba.com>
2014-02-16 06:53:43 -0500

    convenient tls serving without keys and certs in files; clarify licensing issues

---
+++ gtls.go
@@ -0,0 +1,34 @@
+package goutil
+
+import (
+	"crypto/tls"
+	"net"
+	"net/http"
+)
+
+// adapted from go's net/http package, modified to work with bytes in memory instead of files on disk
+func ListenAndServeTLS(srv *http.Server, certFile, keyFile []byte) error {
+	addr := srv.Addr
+	if addr == "" {
+		addr = ":https"
+	}
+	config := &tls.Config{}
+	if srv.TLSConfig != nil {
+		*config = *srv.TLSConfig
+	}
+	if config.NextProtos == nil {
+		config.NextProtos = []string{"http/1.1"}
+	}
+	var err error
+	config.Certificates = make([]tls.Certificate, 1)
+	config.Certificates[0], err = tls.X509KeyPair(certFile, keyFile)
+	if err != nil {
+		return err
+	}
+	conn, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+	tlsListener := tls.NewListener(conn, config)
+	return srv.Serve(tlsListener)
+}
