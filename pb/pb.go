0bccac25c9629af894b85d907367770adc00830a
mike andrews <mra@xoba.com>
2014-09-05 09:09:05 -0400

    split up the calc a bit

--- pb/pb.go
+++ pb/pb.go
@@ -47,17 +47,19 @@ func Iterate(pr Problem, weights []float64, lr float64) error {
 	return nil
 }
 
-func LogisticRisk(pr Problem, weights []float64) float64 {
-	var out float64
+func LogisticRisk(pr Problem, weights []float64) (out float64) {
 	n := len(pr.Observations)
-	p := len(weights)
 	for i := 0; i < n; i++ {
-		y := float64(pr.Responses[i])
-		var f float64
-		for j := 0; j < p; j++ {
-			f += weights[j] * pr.Observations[i][j]
-		}
-		out += math.Log(1 + math.Exp(-y*f))
+		out += LogisticLoss(pr.Responses[i], pr.Observations[i], weights)
 	}
 	return out / float64(n)
+}
+
+func LogisticLoss(y float64, f Features, w []float64) float64 {
+	p := len(w)
+	var x float64
+	for j := 0; j < p; j++ {
+		x += w[j] * f[j]
+	}
+	return math.Log(1 + math.Exp(-y*x))
 }
